d3fc7919f13daad19652323f5eba70ee944269a1
zxcneveronsky <stafeewgrisha@gmail.com>
2026-05-04 15:03:24 +0500

    обновление логики делит юзер юзкейса исходя логики sql скриптов с каскадным удалением

--- src/main/java/com/example/fitness_assistant/application/service/user/DeleteUserUseCase.java
+++ src/main/java/com/example/fitness_assistant/application/service/user/DeleteUserUseCase.java
@@ -1,7 +1,6 @@
 package com.example.fitness_assistant.application.service.user;
 
 import com.example.fitness_assistant.core.exception.UserNotFoundException;
-import com.example.fitness_assistant.core.repository.UserProfileRepository;
 import com.example.fitness_assistant.core.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,14 +11,10 @@ import org.springframework.transaction.annotation.Transactional;
 public class DeleteUserUseCase {
 
     private final UserRepository userRepository;
-    private final UserProfileRepository userProfileRepository;
     @Transactional
     public void deleteById(Long id) {
         if (!userRepository.existsById(id)) {
             throw new UserNotFoundException(id);
-        }
-        if (userProfileRepository.existsById(id)){
-            userProfileRepository.deleteById(id);
         }
         userRepository.deleteById(id);
     }
